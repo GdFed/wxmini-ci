61f0e31e878859830bf952d995208695d9dfa75e
xiaozhilong <602742646@qq.com>
2020-10-19 19:08:54 +0800

    github

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    // ci github again
-    // ci github again
-    // ci github again
-    // ci github again
+    // github
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
